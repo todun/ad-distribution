0fb0d447259cd02b6513a2e340a0034298d14fa9
dwai <dwai@ualberta.ca>
2019-01-19 16:54:21 -0700

    React

--- src/my-app/src/App.js
+++ src/my-app/src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import LoginComponent from "./LoginStuff/loginComponent"
+// import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './MainStuff/navigationBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <header className="NavigationBar">
+            <NavigationBar/>
+          </header>
+
+          <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            {/* <h1>
+                Ad Distro
+            </h1> */}
+            {/* <LoginComponent/> */}
+          </header>
       </div>
     );
   }
